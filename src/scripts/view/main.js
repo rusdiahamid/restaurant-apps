79e3c27980a6a2f52c701276ba7124006ad801d7
rusdiahamid <rusdia.hamid@gmail.com>
2022-12-08 06:43:24 +0700

    ok

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,28 +1,27 @@
 import data from '../data/DATA.json';
-import '../component/restaurant-list.js'
+import '../component/restaurant-list';
 
 const main = () => {
-    const menu = document.querySelector('#menu');
-    const body = document.querySelector('body');
-    const main = document.querySelector('main');
-    const drawer = document.querySelector('nav-bar');
+  const menu = document.querySelector('#menu');
+  const body = document.querySelector('body');
+  const main = document.querySelector('main');
+  const drawer = document.querySelector('nav-bar');
 
-    menu.addEventListener('click', function (event) {
-        drawer.classList.toggle('open');
-        event.stopPropagation();
-    });
+  menu.addEventListener('click', (event) => {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+  });
 
-    body.addEventListener('click', function () {
-        drawer.classList.remove('open');
-    });
+  body.addEventListener('click', () => {
+    drawer.classList.remove('open');
+  });
 
-    main.addEventListener('click', function () {
-        drawer.classList.remove('open');
-    });
+  main.addEventListener('click', () => {
+    drawer.classList.remove('open');
+  });
 
-    const restoListElement = document.querySelector('restaurant-list');
-    restoListElement.restaurants = data.restaurants;
-
+  const restoListElement = document.querySelector('restaurant-list');
+  restoListElement.restaurants = data.restaurants;
 };
 
 export default main;
