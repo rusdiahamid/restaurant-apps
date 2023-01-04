b5e42b23365feb22e2d6cb5e0b5a2768670fb477
rusdiahamid <rusdia.hamid@gmail.com>
2023-01-04 12:39:37 +0700

    ok

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: {
